085aab4c3aeb1b908344fd7bca283bdc147d9b81
Krati96 <kv.krati.varshney@gmail.com>
2024-04-16 11:42:56 +0530

    update userList

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,39 +1,3 @@
-// // UserListPage.js
-// import React, { useState } from 'react';
-// import { Link } from 'react-router-dom';
-
-// function UserListPage() {
-//   const [users, setUsers] = useState([...]); // Initial user list
-//   const [currentPage, setCurrentPage] = useState(1);
-//   const usersPerPage = 10;
-//   const indexOfLastUser = currentPage * usersPerPage;
-//   const indexOfFirstUser = indexOfLastUser - usersPerPage;
-//   const currentUsers = users.slice(indexOfFirstUser, indexOfLastUser);
-
-//   const paginate = (pageNumber) => setCurrentPage(pageNumber);
-
-//   return (
-//     <div>
-//       <h1>User List</h1>
-//       <ul>
-//         {currentUsers.map(user => (
-//           <li key={user.id}>
-//             <Link to={`/users/${user.id}`}>{user.username}</Link>
-//           </li>
-//         ))}
-//       </ul>
-//       {/* <Pagination
-//         usersPerPage={usersPerPage}
-//         totalUsers={users.length}
-//         paginate={paginate}
-//       /> */}
-//     </div>
-//   );
-// }
-
-// export default UserListPage;
-
-// UserList.js
 import React from 'react';
 import { Link } from 'react-router-dom';
 
